c46b0f012338879a96b8cd4c8426599042bd36f9
Audrey van den Berg <audreybergdev@gmail.com>
2022-02-23 16:16:46 +0100

    randomn trees

--- scene.js
+++ scene.js
@@ -9,22 +9,38 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer({
    antialias: true,
    alpha: true });
+renderer.shadowMap.enabled = true;
+
+// const light = new THREE.DirectionalLight(color, intensity);
+// light.castShadow = true;
+const skyColor = 0xB1E1FF;  // light blue
+const groundColor = 0xB97A20;  // brownish orange
+const intensity = 2;
+const light = new THREE.DirectionalLight(skyColor, intensity);
+light.castShadow = false;
+
+scene.add(light);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//set a plane for tree
-const mPlane = new THREE.MeshBasicMaterial({color: 'rgb(153, 255, 204)'});  
-const wPlane = 20;  // ui: width
-const hPlane = 10;  // ui: height
-const gPlane = new THREE.PlaneGeometry(
-    wPlane, hPlane);
+renderPlane();
+function renderPlane() {
+    //set a plane for tree
+    const mPlane = new THREE.MeshBasicMaterial({color: 'rgb(153, 255, 204)'});  
+    const wPlane = 20;  // ui: width
+    const hPlane = 10;  // ui: height
+    const gPlane = new THREE.PlaneGeometry(
+        wPlane, hPlane);
 
-const plane = new THREE.Mesh(gPlane, mPlane);
-plane.rotation.z = 6;
-plane.rotation.x = 5;
+    const plane = new THREE.Mesh(gPlane, mPlane);
+    plane.rotation.z = 0;
+    plane.rotation.y = 0;
+    plane.rotation.x = 5;
 
-scene.add(plane);
+    scene.add(plane);
+}
+
 // Move camera from center
 camera.position.x = 0; // move right from center of scene
 camera.position.y = 0; // move up from center of scene
@@ -50,10 +66,56 @@ camera.position.z = 5; // move camera away from center of scene
 // const cone = new THREE.Mesh( geometry, material );
 // scene.add( cone );
 
-const geometry = new THREE.CylinderGeometry( 2, 2, 5, 32 );
-const material = new THREE.MeshBasicMaterial( {color: 'rgb(153, 102, 0)'} );
-const cylinder = new THREE.Mesh( geometry, material );
-scene.add( cylinder );
+// const geometry = new THREE.CylinderGeometry( 2, 2, 5, 32, 1 );
+// const material = new THREE.MeshBasicMaterial( {color: 'rgb(153, 102, 0)'} );
+// const cylinder = new THREE.Mesh( geometry, material );
+// scene.add( cylinder );
+treeMesh();
+function treeMesh() {
+    var loader = new THREE.TextureLoader();
+    
+    for(var i = 0; i < 5; i++) {
+        const randomPosition = Math.random()*20-5; 
+        let log = new THREE.BoxGeometry(0.1,1,0.1);
+        let logMat = new THREE.MeshBasicMaterial( {color: 'rgb(153, 102, 0)'})
+        let logMesh = new THREE.Mesh(log, logMat);
+        logMesh.position.y = 0.5;
+        logMesh.position.x = randomPosition;
+        // logMesh.position
+        logMesh.castShadow = true;
+        logMesh.receiveShadow = true;
+        console.log("x=" , logMesh.position.x, "y position=", logMesh.position.y, );
+        
+        scene.add(logMesh);
+    
+        const radius = 0.3;  // ui: radius
+        const widthSegments = 12;  // ui: widthSegments
+        const heightSegments = 8;  // ui: heightSegments
+        const sphere = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+    
+        let sphereMaterial = new THREE.MeshBasicMaterial({color: 'rgb(0, 179, 89)'});
+        const sphereMeshOne = new THREE.Mesh(sphere, sphereMaterial);
+        const sphereMeshTwo = new THREE.Mesh(sphere, sphereMaterial);
+        const sphereMeshTree = new THREE.Mesh(sphere, sphereMaterial);
+        const sphereMeshFour = new THREE.Mesh(sphere, sphereMaterial);
+    
+        sphereMeshOne.position.y = 1.5;
+        sphereMeshTwo.position.y = 1.5;
+        sphereMeshTree.position.y = 1.8;
+        sphereMeshFour.position.y = 1.2;
+        sphereMeshOne.position.x = randomPosition - 0.2;
+        // sphereMeshTwo.position.x = -0.15;
+        sphereMeshTwo.position.x = randomPosition - 0.15;
+        sphereMeshTree.position.x = randomPosition;
+        sphereMeshFour.position.x = randomPosition; 
+    
+    
+    
+        scene.add(sphereMeshOne, sphereMeshTwo, sphereMeshTree,sphereMeshFour);
+    }
+}
+
+
 // - a circle of radius 5 on top (1st parameter)
 // - a circle of radius 5 on the bottom (2nd parameter)
 // - a height of 20 (3rd parameter)
